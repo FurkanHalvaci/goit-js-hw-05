5cff52ca49fdbb4b8c40b5a1ae05cffc689754a5
FurkanHalvaci <98705797+FurkanHalvaci@users.noreply.github.com>
2025-10-20 14:43:30 +0300

    done

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const getTotalBalanceByGender = (users, gender) => {
     return user.gender === gender ? total + user.balance : total;
   }, 0);
 };
-const allUsers = [
+const kullanıciler = [
 	{
     name: "Moore Hensley",
     gender: "male",
@@ -41,6 +41,6 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(kullanıciler, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(kullanıciler, "female")); // 8863
